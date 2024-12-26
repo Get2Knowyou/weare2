6692b6629ba5ea7695bf87db9cb2c74e9afc6e41
hyerin-kang <hkang0420@gmail.com>
2024-12-26 20:13:33 +0900

    방명록 삭제기능 추가중

--- miniproject/visitors/visitorsFirebase.js
+++ miniproject/visitors/visitorsFirebase.js
@@ -1,15 +1,14 @@
-  //방명록 작성 폼 보이게 하기기
-  document.getElementById("openWrite").addEventListener("click", function () {
+//방명록 작성 폼 보이게 하기기
+document.getElementById("openWrite").addEventListener("click", function () {
     document.getElementById("sendMsg").classList.toggle('disBlock');
-  })
-
-  
+})
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 import { collection, addDoc } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 import { getDocs } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+import {doc,deleteDoc} from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -47,10 +46,9 @@ function createLi(visitor) {
     const commentList = document.getElementById("commentList");
     let li = document.createElement("li");
     li.innerHTML = `
-      <p class="name">${visitor.name}</p>
-      <p class="memo">${visitor.message}</p>
-      <button>삭제</button>
-    `;
+    <p class="name">${visitor.name}</p>
+    <p class="memo">${visitor.message}</p>
+    <button class="deleteBtn">삭제</button>`;
     commentList.appendChild(li);
 }
 
@@ -73,10 +71,9 @@ const gestForm = document.getElementById("sendMsg").addEventListener("submit", a
         //컴포넌트 생성
         let li = document.createElement("li");
         li.innerHTML = `
-          <p class="name">${name}</p>
-          <p class="memo">${message}</p>
-          <button>삭제</button>
-      `;
+        <p class="name">${name}</p>
+        <p class="memo">${message}</p>
+        <button class="deleteBtn">삭제</button>`;
         document.getElementById("commentList").prepend(li);
     } else {
         if (!name) {
@@ -99,4 +96,24 @@ const gestForm = document.getElementById("sendMsg").addEventListener("submit", a
     e.target.name.value = '';
     e.target.pw.value = '';
     e.target.message.value = '';
+})
+
+//삭제버튼
+async function deleteLi(id,pw,li){
+    const userPw = prompt("비밀번호를 입력하세요");
+
+    if( userPw !== pw){
+        alert("비밀번호가 일치하지 않습니다.");
+        return;
+    }
+}
+
+//삭제버튼
+$("#deletebtn").click(async function () {
+    let goal = $('${goalId}').val();
+    console.log(goal);
+    
+    await deleteDoc(doc(db, "goals", "goal"));
+    alert('삭제 완료!');
+    window.location.reload();
 })
